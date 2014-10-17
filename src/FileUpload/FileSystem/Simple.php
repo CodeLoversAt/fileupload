f9c178fdb9ccdba3d93df8c050fd15cdac52821b
Daniel Holzmann <d@velopment.at>
2014-10-18 00:26:43 +0200

    using rename instead of copy to move uploaded file

--- src/FileUpload/FileSystem/Simple.php
+++ src/FileUpload/FileSystem/Simple.php
@@ -28,7 +28,7 @@ class Simple implements FileSystem {
    * @see FileSystem
    */
   public function moveUploadedFile($from_path, $to_path) {
-    return copy($from_path, $to_path) && unlink($from_path);
+    return rename($from_path, $to_path);
   }
 
   /**
